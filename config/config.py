33e86bfea358f1b0d9b1ba27e1d505debd721f7f
Starboymusic <visionm59@gmail.com>
2024-10-26 17:08:58 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -64,7 +64,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheChampu/ChampuMusic",
+    "https://github.com/Starboymusic/VisionChampuMusic1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
